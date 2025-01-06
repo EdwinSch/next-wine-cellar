ab3f5932d59587acb34d35d81c81ad181b79373a
Edwin <edwinschutjes@gmail.com>
2025-01-06 17:21:53 +0100

    init prj

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "My Wine Cellar",
+  keywords: "wine, collection, cellar",
+  description: "My wine collection",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
